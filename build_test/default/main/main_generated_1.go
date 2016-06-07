b130083445dcbb28bb7dbcb55580f2b818077b1d
Sven Andersson <sven.andersson@king.com>
2016-06-07 12:09:32 +0200

    Test

---
+++ build_test/default/main/main_generated_1.go
@@ -0,0 +1,16 @@
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_STATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"default\"\nmask: \"default\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: 150.0\n      y: -10.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 3\n  }\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: -10.0\n      y: 100.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 3\n    count: 3\n  }\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: 310.0\n      y: 100.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 6\n    count: 3\n  }\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: 150.0\n      y: 210.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 9\n    count: 3\n  }\n  data: 150.0\n  data: 10.0\n  data: 10.0\n  data: 10.0\n  data: 100.0\n  data: 10.0\n  data: 10.0\n  data: 100.0\n  data: 10.0\n  data: 150.0\n  data: 10.0\n  data: 10.0\n}\nlinear_damping: 0.0\nangular_damping: 0.0\nlocked_rotation: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
